0854b2cb1ede9f1ae5857c3f2e67bf3cd40c6f21
maazil3861 <maazshaikh7020@gmail.com>
2020-04-05 05:24:15 -0700

    Inject value using setter method

---
+++ inject using setter/src/Injection/Exam.java
@@ -0,0 +1,23 @@
+package Injection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Exam {
+
+	public static void main(String[] args) { 
+		
+	
+		
+		
+		// inject value using setter method and xml file 
+		
+		  ApplicationContext context = new ClassPathXmlApplicationContext("NewFile.xml");  	 
+		  Student obj = context.getBean("Shaikh",Student.class);
+		  obj.Dislapy();
+		   	 
+		  Student obj1 = context.getBean("EnterValue",Student.class);
+		  obj1.Dislapy();
+
+	}
+
+}
