98da67ab8da426b000a41e366a8ad12a65a08de0
maazil3861 <maazshaikh7020@gmail.com>
2020-04-06 02:02:06 -0700

    autowiring byName means class objec Name you can access

---
+++ Annotation using autowiring byName/src/part6/Body.java
@@ -0,0 +1,13 @@
+package part6;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Body {
+
+	public  static void main(String args[]) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("NewFile.xml"); 
+		Human human = context.getBean("human",Human.class); 
+		human.startPumping();
+	}
+}
